c7bd271675550e3dc5ee46da0151f3ac2e6a2008
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-05-26 16:03:47 -0400

    se crea el metodo getEmployeesByBirthdate para que lea el archivo y filtre los empleados

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -6,17 +6,7 @@ export class BirthdayService {
   constructor() {}
 
   sendGreetings(ourDate, fileName, smtpUrl, smtpPort, transport) {
-    const data = fs.readFileSync(
-      path.resolve(__dirname, `${fileName}`), //`../${fileName}`),
-      "UTF-8"
-    );
-
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    lines.shift();
-    const employees = lines
-      .map((line) => this.createEmployeeFromLine(line))
-      .filter((employee) => employee.isBirthday(ourDate));
+    const employees = this.getEmployeesByBirthDate(ourDate, fileName);
 
     employees.forEach((employee) => {
       const message = {
@@ -29,6 +19,23 @@ export class BirthdayService {
       };
       transport.sendMail(message);
     });
+  }
+
+  getEmployeesByBirthDate(ourDate, fileName) {
+    const data = fs.readFileSync(
+      path.resolve(__dirname, `${fileName}`),
+      "UTF-8"
+    );
+
+    // split the contents by new line
+    const lines = data.split(/\r?\n/);
+    lines.shift();
+
+    const employees = lines
+      .map((line) => this.createEmployeeFromLine(line))
+      .filter((employee) => employee.isBirthday(ourDate));
+    
+    return employees;
   }
 
   createEmployeeFromLine(line) {
